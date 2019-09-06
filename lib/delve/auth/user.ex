ce097870f492cb95268e650bfc6dbe38d3d48217
Eric Mueller <nevinera@gmail.com>
2019-09-05 21:41:45 -0400

    generate the user model

---
+++ lib/delve/auth/user.ex
@@ -0,0 +1,20 @@
+defmodule Delve.Auth.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :username])
+    |> validate_required([:email, :username])
+    |> unique_constraint(:email)
+    |> unique_constraint(:username)
+  end
+end
