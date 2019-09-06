9b3b2e6b0c725a36aff86d9140738dd64e753d2e
Eric Mueller <nevinera@gmail.com>
2019-09-05 21:56:54 -0400

    remove not-null constraint from password_hash - handle null hash as not matching anything

--- priv/repo/migrations/20190906013957_create_users.exs
+++ priv/repo/migrations/20190906013957_create_users.exs
@@ -5,7 +5,7 @@ defmodule Delve.Repo.Migrations.CreateUsers do
     create table(:users) do
       add :email, :string, null: false
       add :username, :string, null: false
-      add :password_hash, :string, null: false
+      add :password_hash, :string
 
       timestamps()
     end
