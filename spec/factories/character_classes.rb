7a4b5943e9d8a951286f4c7cf68cf35a721cbce7
Eric Mueller <nevinera@gmail.com>
2026-08-26 13:13:12 -0400

    feat: show primary stats column on character classes index
    Also gives the character_class factory valid default primary/secondary
    stats and a :hybrid trait for classes with multiple primary stats.

--- spec/factories/character_classes.rb
+++ spec/factories/character_classes.rb
@@ -5,5 +5,11 @@ FactoryBot.define do
     sequence(:identifier) { |n| "class#{n.to_s.rjust(3, "0")}" }
     version { "1.0" }
     location { "https://github.com/example/delve/blob/main/docs/examples/classes/puncher.json" }
+    primary_stats { ["strength"] }
+    secondary_stats { %w[stamina crit_rating haste_rating mastery_rating versatility_rating] }
+
+    trait :hybrid do
+      primary_stats { %w[strength intellect] }
+    end
   end
 end
