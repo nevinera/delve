6d317b3f860cdb4b05047fadf2c934d6b54cf5fe
Eric Mueller <nevinera@gmail.com>
2026-09-19 14:06:27 -0400

    Extract character settings assignment to satisfy rubocop AbcSize

--- app/controllers/play/zones_controller.rb
+++ app/controllers/play/zones_controller.rb
@@ -14,8 +14,6 @@ class Play::ZonesController < Play::BaseController
     @result = JoinZone.call(character: @character, zone: @zone)
     @owned_zone_items = owned_zone_items_map
     @equipped_items = EquippedItems::ForCharacter.call(character: @character)
-    @character_settings = @character.setting_or_default.as_client_json
-    @character_settings_url = play_character_setting_path(@character)
     assign_equipment_urls
     assign_stock_assets
   end
@@ -29,6 +27,12 @@ class Play::ZonesController < Play::BaseController
   def assign_equipment_urls
     @character_items_url = play_character_character_items_path(@character, format: :json)
     @equipped_items_url = play_character_equipped_items_path(@character)
+    assign_character_settings
+  end
+
+  def assign_character_settings
+    @character_settings = @character.setting_or_default.as_client_json
+    @character_settings_url = play_character_setting_path(@character)
   end
 
   def assign_stock_assets
